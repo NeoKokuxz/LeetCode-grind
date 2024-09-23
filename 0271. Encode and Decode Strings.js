faf51af87d7d1e8896e4b4b927d79ffed49c21e0
Neo Chen <43288434+NeoKokuxz@users.noreply.github.com>
2024-09-22 21:48:14 -0400

    Update 0271. Encode and Decode Strings.js

--- 0271. Encode and Decode Strings.js
+++ 0271. Encode and Decode Strings.js
@@ -11,25 +11,42 @@ class Solution {
         return encodedString
     }
 
-// 4#neet4#code4#love3#you
-
     /**
      * @param {string} str
      * @returns {string[]}
      */
     decode(str) {
-        let decoded = []
-        let index = 0; // Start position
+        let decoded = [] //output
+        let index = 0; // Start pointer position
         while ( index < str.length) {
-            // Find the next # based on the current index
-            let pos = str.indexOf('#', index)  
-            let length = Number(str.slice(index, pos))
-            index = pos + 1 // Where word starts 
-            let word = str.slice(index, index + length)
-            decoded.push(word)
-            // Update the index to the last word position 
-            index += length 
+            // Check first # position   
+            let position = str.indexOf('#', index)
+            // Check word count before # 
+            let wordCount = Number(str.slice(index, position))
+            // Move index to # position + 1
+            index = position + 1 
+            // Put the index and wordCount into decoded array
+            decoded.push(str.slice(index, index + wordCount))
+            // Put index at the last position of the current word
+            index += wordCount
         }
         return decoded
     }
 }
+
+// 4#neet4#code4#love3#you
+
+// ["neet","code","love","you|too"]
+// 
+// Check the first seprator # 
+// 2nd point to hold the current index which is 0
+
+
+// 4#neet4#code4#love6#you|too
+// First pointer (Find #) => 1 + 4 stop at 5
+// Second pointer (Find word length) 0,1 => word length 
+// 0, 1 => turn it to a number 400 
+
+// First pointer at 3 (the first #) + 1
+// output = ['neet', ]
+
